abadd11dfac8b1de483296430389333d6056c02d
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-10 12:29:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import os
+import configparser
+from telethon import TelegramClient, events
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+print("Initializing configuration...")
+config = configparser.ConfigParser()
+config.read('config.ini')
+API_ID = config.get('default', 'api_id')
+API_HASH = config.get('default', 'api_hash')
+BOT_TOKEN = config.get('default', 'bot_token')
+DATABASE_NAME = config.get('default', "db_name")
+COLLECTION_NAME = config.get('default', "collection_name")
+client = TelegramClient('Bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+# Connect to MongoDB
+url = config.get('default', "MONGO_URI")
+cluster = MongoClient(url)
+db = cluster[DATABASE_NAME]
+HexaDb = db[COLLECTION_NAME]
+@client.on(events.NewMessage(pattern="(?i)/start"))
+async def start(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    text = "hi i am Hexa details. A bot to store datas of tournament of Hexa battle. CREATED BY:- R3AP3R editz"
+    await client.send_message(SENDER, text)
+@client.on(events.NewMessage(pattern="(?i)/add"))
+async def add(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    list_of_words = event.message.text.split(" ")
+    collection = HexaDb
+    uid = list_of_words[1]
+    win = int(list_of_words[2])
+    team = list_of_words[3]
+    post_dict = {"uid": uid, "win": win, "team": team}
+    collection.insert_one(post_dict)
+    text = "details of the player has been inserted!"
+    await client.send_message(SENDER, text)
+@client.on(events.NewMessage(pattern="(?i)/list"))
+async def select(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    list_of_words = event.message.text.split(" ")
+    collection = HexaDb
+    if len(list_of_words) > 1:
+        team = list_of_words[1]
+        results = collection.find({"team": team})
+    else:
+        results = collection.find({})
+    message = create_message_select_query(results)
+    await client.send_message(SENDER, message, parse_mode='html')
+@client.on(events.NewMessage(pattern="(?i)/win"))
+async def update(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    list_of_words = event.message.text.split(" ")
+    collection = HexaDb
+    _id = ObjectId(list_of_words[1])
+    uid = list_of_words[2]
+    win = int(list_of_words[3])
+    team = list_of_words[4]
+    new_post_dict = {"uid": uid, "win": win, "team": team}
+    collection.update_one({"_id": _id}, {"$set": new_post_dict})
+    text = "Product with _id {} correctly updated".format(_id)
+    await client.send_message(SENDER, text, parse_mode='html')
+@client.on(events.NewMessage(pattern="(?i)/remove"))
+async def delete(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    list_of_words = event.message.text.split(" ")
+    collection = HexaDb
+    uid = list_of_words[1]
+    collection.delete_one({"uid": uid})
+    text = "user {} has been removed".format(uid)
+    await client.send_message(SENDER, text, parse_mode='html')
+@client.on(events.NewMessage(pattern="(?i)/in"))
+async def select(event):
+    sender = await event.get_sender()
+    SENDER = sender.id
+    list_of_words = event.message.text.split(" ")
+    collection = HexaDb
+    if len(list_of_words) > 1:
+        field = list_of_words[1]
+        values_to_check = list_of_words[2:]
+        
+        params = {field: {"$in": values_to_check}}
+        results = collection.find(params)
+        message = create_message_select_query(results)
+        await client.send_message(SENDER, message, parse_mode='html')
+def create_message_select_query(results):
+    text = ""
+    for res in results:
+        id = res["_id"]
+        uid = res["uid"]
+        win = res["win"]
+        team = res["team"]
+        text += "<b>"+ str(id) +"</b> | " + "<b>"+ str(uid) +"</b> | " + "<b>"+ str(win)+"</b> | " + "<b>"+ str(team)+"</b> | " + "</b>\n"
+    message = "<b>Received 📖 </b> Information about participants:\n\n"+text
+    return message
+if __name__ == '__main__':
+    try:
+        print("Initializing Database...")
+        # Define the Database using Database name
+        db = cluster[DATABASE_NAME]
+        # Define collection
+        HexaDb = db[COLLECTION_NAME]
+        print("Bot's engine started. Engine is makinging sound!!!....:nigga nigga ")
+        client.run_until_disconnected()
+    except Exception as error:
+        print('Cause: {}'.format(error))
