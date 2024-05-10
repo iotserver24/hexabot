1e216f51d55928c56f1392d3dc69331667d36e8d
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-05-10 15:35:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
-import os
 import configparser
-from telethon import TelegramClient, events
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from pyrogram import Client, filters
+
 print("Initializing configuration...")
 config = configparser.ConfigParser()
 config.read('config.ini')
+
 API_ID = config.get('default', 'api_id')
 API_HASH = config.get('default', 'api_hash')
 BOT_TOKEN = config.get('default', 'bot_token')
 DATABASE_NAME = config.get('default', "db_name")
 COLLECTION_NAME = config.get('default', "collection_name")
-client = TelegramClient('Bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+
+# Initialize Pyrogram client
+pyro_client = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
 # Connect to MongoDB
 url = config.get('default', "MONGO_URI")
 cluster = MongoClient(url)
 db = cluster[DATABASE_NAME]
 HexaDb = db[COLLECTION_NAME]
-@client.on(events.NewMessage(pattern="(?i)/start"))
-async def start(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    text = "hi i am Hexa details. A bot to store datas of tournament of Hexa battle. CREATED BY:- R3AP3R editz"
-    await client.send_message(SENDER, text)
-@client.on(events.NewMessage(pattern="(?i)/add"))
-async def add(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    list_of_words = event.message.text.split(" ")
-    collection = HexaDb
+
+# Command handlers
+@pyro_client.on_message(filters.command("start"))
+async def start_command(client, message):
+    SENDER = message.chat.id
+    text = "Hi, I am Hexa details bot. I store data of Hexa battle tournaments. Created by R3AP3R editz."
+    await pyro_client.send_message(SENDER, text)
+
+@pyro_client.on_message(filters.command("add"))
+async def add_command(client, message):
+    SENDER = message.chat.id
+    list_of_words = message.text.split(" ")
     uid = list_of_words[1]
     win = int(list_of_words[2])
     team = list_of_words[3]
     post_dict = {"uid": uid, "win": win, "team": team}
+    collection = HexaDb
     collection.insert_one(post_dict)
-    text = "details of the player has been inserted!"
-    await client.send_message(SENDER, text)
-@client.on(events.NewMessage(pattern="(?i)/list"))
-async def select(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    list_of_words = event.message.text.split(" ")
+    text = "Details of the player have been inserted!"
+    await pyro_client.send_message(SENDER, text)
+
+@pyro_client.on_message(filters.command("list"))
+async def list_command(client, message):
+    SENDER = message.chat.id
+    list_of_words = message.text.split(" ")
     collection = HexaDb
     if len(list_of_words) > 1:
         team = list_of_words[1]
@@ -48,12 +53,12 @@ async def select(event):
     else:
         results = collection.find({})
     message = create_message_select_query(results)
-    await client.send_message(SENDER, message, parse_mode='html')
-@client.on(events.NewMessage(pattern="(?i)/win"))
-async def update(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    list_of_words = event.message.text.split(" ")
+    await pyro_client.send_message(SENDER, message, parse_mode='html')
+
+@pyro_client.on_message(filters.command("win"))
+async def win_command(client, message):
+    SENDER = message.chat.id
+    list_of_words = message.text.split(" ")
     collection = HexaDb
     _id = ObjectId(list_of_words[1])
     uid = list_of_words[2]
@@ -61,42 +66,42 @@ async def update(event):
     team = list_of_words[4]
     new_post_dict = {"uid": uid, "win": win, "team": team}
     collection.update_one({"_id": _id}, {"$set": new_post_dict})
-    text = "Product with _id {} correctly updated".format(_id)
-    await client.send_message(SENDER, text, parse_mode='html')
-@client.on(events.NewMessage(pattern="(?i)/remove"))
-async def delete(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    list_of_words = event.message.text.split(" ")
+    text = "Player with _id {} has been updated.".format(_id)
+    await pyro_client.send_message(SENDER, text)
+
+@pyro_client.on_message(filters.command("remove"))
+async def remove_command(client, message):
+    SENDER = message.chat.id
+    list_of_words = message.text.split(" ")
     collection = HexaDb
     uid = list_of_words[1]
     collection.delete_one({"uid": uid})
-    text = "user {} has been removed".format(uid)
-    await client.send_message(SENDER, text, parse_mode='html')
-@client.on(events.NewMessage(pattern="(?i)/in"))
-async def select(event):
-    sender = await event.get_sender()
-    SENDER = sender.id
-    list_of_words = event.message.text.split(" ")
+    text = "User {} has been removed.".format(uid)
+    await pyro_client.send_message(SENDER, text)
+
+@pyro_client.on_message(filters.command("in"))
+async def in_command(client, message):
+    SENDER = message.chat.id
+    list_of_words = message.text.split(" ")
     collection = HexaDb
     if len(list_of_words) > 1:
         field = list_of_words[1]
         values_to_check = list_of_words[2:]
-        
         params = {field: {"$in": values_to_check}}
         results = collection.find(params)
         message = create_message_select_query(results)
-        await client.send_message(SENDER, message, parse_mode='html')
+        await pyro_client.send_message(SENDER, message, parse_mode='html')
+
 def create_message_select_query(results):
     text = ""
     for res in results:
-        id = res["_id"]
+        _id = res["_id"]
         uid = res["uid"]
         win = res["win"]
         team = res["team"]
-        text += "<b>"+ str(id) +"</b> | " + "<b>"+ str(uid) +"</b> | " + "<b>"+ str(win)+"</b> | " + "<b>"+ str(team)+"</b> | " + "</b>\n"
-    message = "<b>Received 📖 </b> Information about participants:\n\n"+text
+        text += "<b>" + str(_id) + "</b> | " + "<b>" + str(uid) + "</b> | " + "<b>" + str(win) + "</b> | " + "<b>" + str(team) + "</b> | " + "\n"
+    message = "<b>Received 📖 </b> Information about participants:\n\n" + text
     return message
-        
 
-client.run_until_disconnected()
+# Run Pyrogram client
+pyro_client.run()
